4003cdfeb68bd9537b9defe15beca95c6a7a71ae
Marc Sibert <marc@sibert.fr>
2019-03-22 08:02:51 +0100

    Ajout des commentaires / doc

--- Papillon.ino
+++ Papillon.ino
@@ -1,18 +1,46 @@
 /**
- * 
+ * Copyright 2019 par Marc SIBERT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @file Papillon.ino Sketch pour IDE Arduino
+ * @license Apache-2.0
+ * @copyright 2019 par Marc SIBERT
+ * @author Marc SIBERT <marc@sibert.fr>
  */
 
+/**
+ * Distribution normale centrée.
+ * 
+ * @param x le paramètre
+ * @return l'amplitude
+ */
 byte f(const float x) {
   return 255 * exp( -sq( x / 2.0f / 0.5f ) );
 }
 
-
+/**
+ * Classe modélisant une ligne de LEDs RGB WS2812.
+ * 
+ * @tparam PIN est le bit du PORT B utilisé pour commander la ligne de LEDs
+ * @tparam LEN est la longueur de la ligne de LEDs
+ */
 template<byte PIN = 1, byte LEN = 1>
 class Line {
 
 public:
 /**
- * Red, Green, Blue byte values.
+ * Structure regroupant les valeurs Red, Green, Blue.
  */
   struct rgb_t {
     byte r;
@@ -21,17 +49,31 @@ public:
   };
   
 /**
- * Should be called in arduino setup func.
- * Initialize ports
+ * À appeler à l'initialisation de l'application.
+ * 
+ * Initialise le port de sortie.
  */
   void setup() {
     DDRB |= _BV(PIN);  
   }
 
+/**
+ * Définit le triplet de la LED indiquée.
+ * 
+ * @warning Il s'agit d'une copie en mémoire. Pour être effectivement appliquée, il faut utiliser la méthode flush()
+ * 
+ * @param pos Position de la LED dans la ligne [0..n-1]
+ * @param rgb Une référence sur les valeurs à appliquer à cette LED
+ */
   void setRGB(const byte pos, const rgb_t& rgb) {
     if (pos < LEN) data[pos] = rgb;
   }
 
+/**
+ * Applique l'ensemble des triplets mémorisés sur la ligne de LEDs.
+ * 
+ * @warning pendant la transmission, les interruptions sont désactivées.
+ */
   void flush() const {
     PORTB = 0;
     delayMicroseconds(50);
@@ -45,6 +87,13 @@ protected:
 #define NOP2 NOP1 NOP1 
 #define NOP5 NOP2 NOP2 NOP1
 
+/**
+ * Transmet un bit modulé sur la ligne pré-définie.
+ * 
+ * @warning Cette méthode est définie pour une fréquence de processeur de 16 MHz.
+ * 
+ * @param b Un booléen indiqauant l'état du bit à transmettre.
+ */
   inline 
   void sendBit(const bool b) const {
     PORTB |= _BV(PIN);
@@ -54,6 +103,13 @@ protected:
     PORTB &= !_BV(PIN);
   }
 
+/**
+ * Transmet le triplet de couleurs vers la ligne.
+ * 
+ * @warning Cette méthode désactive ponctuellement les interruptions.
+ * 
+ * @param rgb Un triplet de couleurs.
+ */
   void sendRGB(const rgb_t& rgb) const {
     cli();
     sendBit(0x80 & rgb.r); 
@@ -90,6 +146,7 @@ protected:
   }
   
 private:
+/// Zone mémoire concervant les triplets de couleur de chaque LED de la ligne.
   rgb_t data[LEN];
   
 };
